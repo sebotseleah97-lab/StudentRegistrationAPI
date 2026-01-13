4297f62c2403fc1c3c6262a5495507c7f76e0c0f
sebotseleah-lab <sebotseleah97@gmail.com>
2026-01-13 11:05:38 +0200

    Student Registration

--- StudentRegistrationAPI/Controllers/StudentsController.cs
+++ StudentRegistrationAPI/Controllers/StudentsController.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;   // Make sure matches your Data folder
+using StudentRegistration.Models; // Must match Models folder
 using StudentRegistrationAPI.Data;
-using StudentRegistration.Models;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace StudentRegistration.Controllers
@@ -13,66 +14,18 @@ namespace StudentRegistration.Controllers
         private readonly ApplicationDbContext _context;
 
         public StudentsController(ApplicationDbContext context)
+
         {
             _context = context;
         }
 
-        // GET: api/Students
         [HttpGet]
-        public async Task<IActionResult> GetAllStudents()
+        public async Task<ActionResult<IEnumerable<Student>>> GetAll()
         {
-            var students = await _context.Students.ToListAsync();
-            return Ok(students);
-        }
+           
 
-        // GET: api/Students/1
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetStudent(int id)
-        {
-            var student = await _context.Students.FindAsync(id);
 
-            if (student == null)
-                return NotFound();
-
-            return Ok(student);
-        }
-
-        // POST: api/Students
-        [HttpPost]
-        public async Task<IActionResult> AddStudent(Student student)
-        {
-            _context.Students.Add(student);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetStudent), new { id = student.Id }, student);
-        }
-
-        // PUT: api/Students/1
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateStudent(int id, Student student)
-        {
-            if (id != student.Id)
-                return BadRequest();
-
-            _context.Entry(student).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        // DELETE: api/Students/1
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteStudent(int id)
-        {
-            var student = await _context.Students.FindAsync(id);
-
-            if (student == null)
-                return NotFound();
-
-            _context.Students.Remove(student);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            return CreatedAtAction(nameof(GetAll), new { id =Students.Id }, Students);
         }
     }
 }
